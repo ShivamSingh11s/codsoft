30b157ca20b368de0928458883b89e70290ca28a
Shivam Singh <147870844+ShivamSingh11s@users.noreply.github.com>
2026-09-13 02:50:59 +0530

    Delete Task3 directory

--- Task3/src/routes/analyticsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const analyticsController = require('../controllers/analyticsController');
-const { authenticate, requireAdmin } = require('../middleware/auth');
-
-router.get('/user', authenticate, analyticsController.getUserStats);
-router.get('/admin', authenticate, requireAdmin, analyticsController.getAdminStats);
-
-module.exports = router;
--- Task3/src/routes/questionRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const questionController = require('../controllers/questionController');
-const { authenticate, requireAdmin } = require('../middleware/auth');
-
-router.put('/:id', authenticate, requireAdmin, questionController.updateQuestion);
-router.delete('/:id', authenticate, requireAdmin, questionController.deleteQuestion);
-
-module.exports = router;
